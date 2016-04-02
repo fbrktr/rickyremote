c31a090fad2596683e2d207f429d8e52ba5ab191
fbrktr <fabrice.benoit@homepulse.fr>
2016-04-02 19:08:49 +0200

    Added application.conf

---
+++ src/main/scala/ParticleStatus.scala
@@ -0,0 +1,62 @@
+import java.net.InetAddress
+
+import akka.actor.{Actor, ActorLogging, ActorRef}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.pattern.pipe
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
+import akka.util.ByteString
+
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
+class ParticleStatus(brain:ActorRef) extends Actor
+  with ActorLogging {
+
+  import context.dispatcher
+  val http = Http(context.system)
+
+  final implicit val materializer: ActorMaterializer = ActorMaterializer(ActorMaterializerSettings(context.system))
+
+  val particleURL = "https://api.particle.io/v1"
+
+  val particleToken = context.system.settings.config.getString("particleToken")
+  val rickyId = context.system.settings.config.getString("particleID")
+
+  val particleTokenSuffix = s"?access_token=$particleToken&format=raw"
+
+  val rickyLocalIpStatusURL = s"$particleURL/devices/$rickyId/localip$particleTokenSuffix"
+
+  var particleStatus = ""
+
+  override def preStart() = {
+    println(context.system.settings.config.getString("particleToken"))
+    updateParticleStatus()
+  }
+
+  def receive = {
+
+    case HttpResponse(StatusCodes.OK, headers, entity, toto) =>
+      val resp: Future[ByteString] = entity.withContentType(ContentTypes.`application/json`).dataBytes.runFold(ByteString(""))(_ ++ _)
+      resp onComplete {
+        case Success(bs) => handleParticleStatus(bs)
+        case Failure(e) => println(e)
+      }
+
+    case HttpResponse(code, _, _, _) =>
+      log.info("Request failed, response code: " + code)
+
+  }
+
+  def updateParticleStatus(): Unit ={
+    http.singleRequest(HttpRequest(uri = rickyLocalIpStatusURL ))
+      .pipeTo(self)
+  }
+
+  def handleParticleStatus(bs:ByteString): Unit ={
+    val rickyIP = bs.decodeString(HttpCharsets.`UTF-8`.value)
+    println(s"Got Ricky's local IP from Particle Cloud : $rickyIP")
+    brain ! InetAddress.getByName(rickyIP)
+  }
+
+}
---
+++ src/main/scala/io/RickyListener.scala
@@ -0,0 +1,36 @@
+package io
+
+import java.net.{InetAddress, InetSocketAddress}
+
+import akka.actor.{Actor, ActorRef}
+import akka.io.{IO, Udp}
+
+/**
+  * Created by fbrktr on 02/04/2016.
+  */
+
+class RickyListener(brain:ActorRef, port:Int) extends Actor {
+
+  import context.system
+  val localAddress = InetAddress.getLocalHost.getHostAddress
+
+  println(s"Binding to $localAddress:$port")
+  IO(Udp) ! Udp.Bind(self, new InetSocketAddress(localAddress, port))
+
+  def receive = {
+    case Udp.Bound(local) =>
+      println(s"Listening to $localAddress:$port")
+      context.become(ready(sender()))
+  }
+
+  def ready(socket: ActorRef): Receive = {
+    //case msg:String => socket ! Udp.Send(ByteString(msg,"UTF-8"), rickySocket)
+    case Udp.Received(data, remote) =>
+      //println(data)
+      //socket ! Udp.Send(ByteString("Bisou\n","UTF-8"), remote)
+      brain ! new RickyMessage(data.decodeString("UTF-8"))
+    case Udp.Unbind  => socket ! Udp.Unbind
+    case Udp.Unbound => context.stop(self)
+  }
+
+}
